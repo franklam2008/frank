abbf9af227c46a1f418386d22143a6dcc4753c30
franklam2008 <franklam2008@gmail.com>
2020-01-24 00:04:58 -0500

    contact form

--- src/components/ContactBtn/ContactLightBox.jsx
+++ src/components/ContactBtn/ContactLightBox.jsx
@@ -1,21 +1,13 @@
 import { Modal, Button } from "react-bootstrap";
-import React, { useRef, useState, InputGroup } from "react";
-import { Spinner, Row, Col, Form } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import { Form, Row, Col } from "react-bootstrap";
+import axios from "axios";
+
 // import Img from "react-image";
 
 export default function LightBox({ show, onHide }) {
-  const [validated, setValidated] = useState(false);
+  const [formMsg, setFormMsg] = useState("");
 
-  const handleSubmit = event => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
-    setValidated(true);
-  };
-  console.log();
   const emailInput = useRef();
   const nameInput = useRef();
   const phoneInput = useRef();
@@ -33,57 +25,51 @@ export default function LightBox({ show, onHide }) {
       <Modal.Header closeButton>Contact Form</Modal.Header>
 
       <Modal.Body>
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Form.Row>
-            <Form.Group as={Col} md="6" controlId="validationCustom01">
-              <Form.Label>First name</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="First name"
-                defaultValue="Mark"
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="6" controlId="validationCustom02">
-              <Form.Label>Last name</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="Last name"
-                defaultValue="Otto"
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-          </Form.Row>
-          <Form.Row>
-            <Form.Group as={Col} md="6" controlId="validationCustom03">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="text" placeholder="Email" required />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid email.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="6" controlId="validationCustom04">
-              <Form.Label>Phone</Form.Label>
-              <Form.Control type="text" placeholder="Phone Number" />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid Phone Number.
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Form.Row>
-          <Form.Group>
-            <Form.Check label="Send me an SMS" />
-          </Form.Group>
-          <Form.Group>
-            <Form.Check
-              required
-              label="Agree to terms and conditions"
-              feedback="You must agree before submitting."
+        <Form>
+          <Form.Group controlId="formBasicPassword">
+            <input
+              className="inputSaved"
+              type="text"
+              ref={nameInput}
+              placeholder="Name"
+              // defaultValue="Frank"
             />
           </Form.Group>
-          <Button type="submit">Submit form</Button>
+          <Form.Group controlId="formBasicEmail">
+            <input
+              className="inputSaved"
+              type="email"
+              ref={emailInput}
+              placeholder="Enter email"
+              defaultValue="email@example.com"
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <input
+              className="inputSaved"
+              type="tel"
+              ref={phoneInput}
+              placeholder="Phone Number"
+              defaultValue="3332222323"
+            />
+          </Form.Group>{" "}
+          <Form.Group controlId="formBasicPassword">
+            <input
+              className="inputSaved"
+              type="text"
+              ref={messageInput}
+              placeholder="Message"
+              defaultValue="Hello World!"
+            />
+          </Form.Group>
+          <Form.Text>
+            We'll never share your information with anyone else.
+          </Form.Text>
+          <Button variant="success" type="submit" onClick={handleForm}>
+            Submit
+          </Button>
         </Form>
+        <p>{formMsg}</p>
       </Modal.Body>
 
       <Modal.Footer>
@@ -96,14 +82,19 @@ export default function LightBox({ show, onHide }) {
   function handleForm(e) {
     console.log("test");
     e.preventDefault();
-    const email = emailInput.current.value;
-    const name = nameInput.current.value;
-    const phone = phoneInput.current.value;
-    const message = messageInput.current.value;
+    const input = {
+      name: nameInput.current.value,
+      email: emailInput.current.value,
+      phone: phoneInput.current.value,
+      message: messageInput.current.value
+    };
 
-    sendForm(name, email, phone, message);
+    sendForm(input);
   }
-  function sendForm(name, email, phone, message) {
-    console.log(name, email, phone, message);
+  function sendForm(input) {
+    axios.post("http://localhost:5000/submit", input).then(res => {
+      console.log(res);
+      setFormMsg(res.data);
+    });
   }
 }
